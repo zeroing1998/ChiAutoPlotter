934eb09b28c0fab7062736ed9af8611e2e4162a8
葛志强 <88195342@qq.com>
2021-07-11 17:18:24 +0800

    去除注册验证

--- Program.cs
+++ Program.cs
@@ -15,40 +15,10 @@ namespace ChiAutoPlotter
         [STAThread]
         static void Main()
         {
-
-
-            reg();
-
-        }
-        static void reg()
-        {
-            var aa = 49489;
-            var cc = 544784;
-            var qq = aa + cc;
             Application.EnableVisualStyles();
+
             Application.SetCompatibleTextRenderingDefault(false);
-            try
-            {
-                if (556 + 4848 == 5404 && qq == aa + cc && File.ReadAllText("reg.key") == ChiAutoPlotter.reg.Reg.get_reg(ChiAutoPlotter.reg.Reg.get_m_code()) && qq - aa == cc)
-                {
-                    var a = 1;
-                    var b = 2;
-                    var c = 3;
-                    Application.Run(new Form1());
-                }
-                else
-                {
-                    new ChiAutoPlotter.reg.RegBox().ShowDialog();
-                    Process.GetCurrentProcess().Kill();
-
-                }
-            }
-            catch (Exception e)
-            {
-                new ChiAutoPlotter.reg.RegBox().ShowDialog();
-                Process.GetCurrentProcess().Kill();
-
-            }
+            Application.Run(new Form1());
         }
     }
 }
